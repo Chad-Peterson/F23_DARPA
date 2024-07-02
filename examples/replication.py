31ee73ecd4a2b412004da9e33b2e3d8c97b8b9f2
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2024-07-02 14:11:21 -0500

    Example updates

--- examples/replication.py
+++ examples/replication.py
@@ -5,60 +5,39 @@ import matplotlib.pyplot as plt
 # %% Define functions
 
 
-def plot_grid(grid, title):
+def plot_bitmap(bitmap, title):
 
     fig, ax = plt.subplots()
 
     ax.set_title(title)
 
-    # Plot the workspace
-    ax.imshow(grid, cmap='gray_r', interpolation='none', extent=[0, grid.shape[1], 0, grid.shape[0]])
+    # Plot the bitmap
+    ax.imshow(bitmap, cmap='gray_r', interpolation='none', extent=[0, bitmap.shape[1], 0, bitmap.shape[0]])
 
     # Draw the grid lines
     ax.grid(True)
 
     # Set the tick locations and labels for the x-axis and y-axis
-    ax.set_xticks(np.arange(0, grid.shape[1]+1, 1))
-    ax.set_yticks(np.arange(0, grid.shape[0]+1, 1))
+    ax.set_xticks(np.arange(0, bitmap.shape[1] + 1, 1))
+    ax.set_yticks(np.arange(0, bitmap.shape[0] + 1, 1))
 
+    # Set the aspect ratio of the plot to be equal
     ax.set_aspect('equal')
 
     # Add color bar
-    cbar = fig.colorbar(ax.imshow(grid, cmap='gray_r', interpolation='none', extent=[0, grid.shape[1], 0, grid.shape[0]]), ax=ax)
+    fig.colorbar(ax.imshow(bitmap, cmap='gray_r', interpolation='none', extent=[0, bitmap.shape[1], 0, bitmap.shape[0]]), ax=ax)
 
     return fig, ax
 
 
-def plot_grid_rgb(grid_rgb, title):
-
-    fig, ax = plt.subplots()
-
-    ax.set_title(title)
-
-    # Plot the workspace
-    # ax.imshow(grid_rgb, interpolation='none', extent=[0, grid_rgb.shape[1], 0, grid_rgb.shape[0]])
-
-
-    # Draw the grid lines
-    ax.grid(True)
-
-    # Set the tick locations and labels for the x-axis and y-axis
-    ax.set_xticks(np.arange(0, grid_rgb.shape[1]+1, 1))
-    ax.set_yticks(np.arange(0, grid_rgb.shape[0]+1, 1))
-
-    ax.set_aspect('equal')
-
-    return fig, ax
-
-
-def plot_fft(fft_result, title, display_boundary=False):
+def plot_fft(fft_coeffs, title):
 
     fig, ax = plt.subplots()
 
     # Shift the zero-frequency component to the center for visualization
-    fft_shifted = np.fft.fftshift(fft_result)
+    fft_shifted = np.fft.fftshift(fft_coeffs)
 
-    # Calculate the magnitude (absolute value)
+    # Calculate the magnitude
     magnitude = np.abs(fft_shifted)
 
     # Scale the magnitude to make it easier to see
@@ -68,186 +47,167 @@ def plot_fft(fft_result, title, display_boundary=False):
 
     ax.set_title(title)
 
-    cbar = fig.colorbar(cax, ax=ax)
-
-    # Draw a boundary around the grid cells near zero
-    if display_boundary:
-
-        threshold = 0.1
-
-        # Create a binary mask for values below the threshold
-        mask = np.where(magnitude_scaled < threshold, 1, 0)
-
-        # Plot the original array
-        ax.imshow(magnitude_scaled, cmap='grey')
-        # plt.colorbar()  # To show the value scale
-
-        # Plot the contour around near-zero values
-        ax.imshow(mask, cmap='Reds', interpolation='none', alpha=0.5)
+    fig.colorbar(cax, ax=ax)
 
     return fig, ax
 
 
-def shift_and_trim_grid(grid, row_shift, col_shift):
-
+def offset_and_trim_bitmap(bitmap, row_offset, col_offset):
     """
-    Shifts the grid by the specified number of rows and columns and trims the excess.
-    :param grid: 2D numpy array
-    :param row_shift: int
-    :param col_shift: int
+    Helper function to combine_bitmaps.
+
+    If you want to combine two same-sized bitmaps, but the center position of one is offset
+    from the center other, then you must trim the non-overlapping parts of one so that the
+    combined bitmap retains the same size as the original bitmaps.
+
+    :param bitmap: 2D numpy array
+    :param row_offset: integer
+    :param col_offset: integer
     :return: 2D numpy array
     """
 
-    n, m = grid.shape
+    n, m = bitmap.shape
 
-    # Handle row shift
-    if row_shift > 0:
-        # Shift down
-        shifted_grid = np.concatenate((np.zeros((row_shift, m)), grid[:-row_shift]), axis=0)
+    # Handle row offset
+    if row_offset > 0:
+        # Offset down
+        offset_bitmap = np.concatenate((np.zeros((row_offset, m)), bitmap[:-row_offset]), axis=0)
     else:
-        # Shift up
-        shifted_grid = np.concatenate((grid[-row_shift:], np.zeros((-row_shift, m))), axis=0)
+        # Offset up
+        offset_bitmap = np.concatenate((bitmap[-row_offset:], np.zeros((-row_offset, m))), axis=0)
 
-    # Handle column shift
-    if col_shift > 0:
-        # Shift right
-        shifted_grid = np.concatenate((np.zeros((n, col_shift)), shifted_grid[:, :-col_shift]), axis=1)
+    # Handle column offset
+    if col_offset > 0:
+        # Offset right
+        offset_bitmap = np.concatenate((np.zeros((n, col_offset)), offset_bitmap[:, :-col_offset]), axis=1)
     else:
-        # Shift left
-        shifted_grid = np.concatenate((shifted_grid[:, -col_shift:], np.zeros((n, -col_shift))), axis=1)
+        # Offset left
+        offset_bitmap = np.concatenate((offset_bitmap[:, -col_offset:], np.zeros((n, -col_offset))), axis=1)
 
-    return shifted_grid
+    return offset_bitmap
 
 
-def merge_grids(grid1, grid2, offset=(0, 0)):
+def combine_bitmaps(bitmap_1, bitmap_2, offset=(0, 0)):
 
-    """Superimpose grid2 on grid1."""
+    """Combine bitmap_1 and bitmap_2. Bitmap_2 may be offset from bitmap_1"""
 
-    # Determine the size of the grids
-    n, m = grid1.shape
+    # Initialize the combined bitmap array as bitmap_1 (the reference bitmap)
+    combined_bitmap = np.array(bitmap_1)
 
-    # Initialize the result array with the first grid
-    result = np.array(grid1)
-
+    # Unpack the offsets
     row_offset, col_offset = offset
 
-    # Shift the second grid
-    shifted_grid2 = shift_and_trim_grid(grid2, row_offset, col_offset)
+    # Offset and trim the second bitmap (see helper function for more details)
+    bitmap_2_offset = offset_and_trim_bitmap(bitmap_2, row_offset, col_offset)
 
     # Add the second grid to the result
-    result += shifted_grid2
+    combined_bitmap += bitmap_2_offset
 
-    return result
+    return combined_bitmap
 
 
-# %% Define the workspace
+# %% Manually define the workspace bitmap (based on Cui et al. Figure 2)
 
-w = np.zeros((16, 16))
+ws = np.zeros((16, 16))
 
-w_rows = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] + \
-         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] + \
-         [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2] + \
-         [3, 3, 3, 3, 3, 3, 3, 3] + \
-         [4, 4, 4, 4, 4, 4] + \
-         [5, 5, 5, 5] + \
-         [6, 6, 6, 6] + \
-         [7, 7, 7, 7] + \
-         [8, 8, 8, 8] + \
-         [9, 9, 9, 9, 9, 9] + \
-         [10, 10, 10, 10, 10, 10] + \
-         [11, 11, 11, 11, 11, 11] + \
-         [12, 12, 12, 12, 12, 12, 12, 12] + \
-         [13, 13, 13, 13, 13, 13, 13, 13] + \
-         [14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14] + \
-         [15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15]
+ws_rows = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] + \
+          [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1] + \
+          [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2] + \
+          [3, 3, 3, 3, 3, 3, 3, 3] + \
+          [4, 4, 4, 4, 4, 4] + \
+          [5, 5, 5, 5] + \
+          [6, 6, 6, 6] + \
+          [7, 7, 7, 7] + \
+          [8, 8, 8, 8] + \
+          [9, 9, 9, 9, 9, 9] + \
+          [10, 10, 10, 10, 10, 10] + \
+          [11, 11, 11, 11, 11, 11] + \
+          [12, 12, 12, 12, 12, 12, 12, 12] + \
+          [13, 13, 13, 13, 13, 13, 13, 13] + \
+          [14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14] + \
+          [15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15]
 
-w_cols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] + \
-         [0, 1, 2, 3, 4, 5, 6, 9, 10, 11, 12, 13, 14, 15] + \
-         [0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15] + \
-         [0, 1, 2, 3, 12, 13, 14, 15] + \
-         [0, 1, 2, 13, 14, 15] + \
-         [0, 1, 14, 15] + \
-         [0, 1, 14, 15] + \
-         [0, 1, 14, 15] + \
-         [0, 1, 14, 15] + \
-         [0, 1, 2, 13, 14, 15] + \
-         [0, 1, 2, 13, 14, 15] + \
-         [0, 1, 2, 13, 14, 15] + \
-         [0, 1, 2, 3, 12, 13, 14, 15] + \
-         [0, 1, 2, 3, 12, 13, 14, 15] + \
-         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] + \
-         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+ws_cols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] + \
+          [0, 1, 2, 3, 4, 5, 6, 9, 10, 11, 12, 13, 14, 15] + \
+          [0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15] + \
+          [0, 1, 2, 3, 12, 13, 14, 15] + \
+          [0, 1, 2, 13, 14, 15] + \
+          [0, 1, 14, 15] + \
+          [0, 1, 14, 15] + \
+          [0, 1, 14, 15] + \
+          [0, 1, 14, 15] + \
+          [0, 1, 2, 13, 14, 15] + \
+          [0, 1, 2, 13, 14, 15] + \
+          [0, 1, 2, 13, 14, 15] + \
+          [0, 1, 2, 3, 12, 13, 14, 15] + \
+          [0, 1, 2, 3, 12, 13, 14, 15] + \
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] + \
+          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-w[w_rows, w_cols] = 1
+ws[ws_rows, ws_cols] = 1
 
 # Plot the workspace
-w_fig, w_ax = plot_grid(w, 'Workspace')
-w_fig.show()
-
-w_rgb = np.stack([1-w, 1-w, 1-w], axis=2)
-w_rgb_fig, w_rgb_ax = plot_grid_rgb(w_rgb, 'Workspace RBG')
-w_rgb_fig.show()
+ws_fig, ws_ax = plot_bitmap(ws, 'Workspace Bitmap')
+ws_fig.show()
 
 
-# %% Define the Object
+# %% Manually define the object bitmap (based on Cui et al. Figure 2)
 
-a = np.zeros((16, 16))
+obj = np.zeros((16, 16))
 
-a_rows = [5, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9]
-a_cols = [6, 6, 7, 6, 7, 8, 6, 7, 8, 9, 6, 7, 8]
+obj_rows = [5, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9]
+obj_cols = [6, 6, 7, 6, 7, 8, 6, 7, 8, 9, 6, 7, 8]
 
-a[a_rows, a_cols] = 1
+obj[obj_rows, obj_cols] = 1
 
 # Plot the object
-a_fig, a_ax = plot_grid(a, 'Object')
-a_fig.show()
+obj_fig, obj_ax = plot_bitmap(obj, 'Object Bitmap')
+obj_fig.show()
 
-# %% Perform the FFTs
+# %% Perform the FFT Convolution of the object and workspace
 
-w_fft = np.fft.fft2(w)
+# Perform the FFT of the workspace
+ws_fft = np.fft.fft2(ws)
 
 # Plot the FFT of the workspace
-w_fft_fig, w_fft_ax = plot_fft(w_fft, 'FFT of Workspace')
-w_fft_fig.show()
+ws_fft_fig, ws_fft_ax = plot_fft(ws_fft, 'FFT of Workspace Bitmap')
+ws_fft_fig.show()
 
-# Flip a first
-a_flipped = np.flip(a,axis=1)
+# Flip the object first (a necessary convention for performing the convolution)
+obj_flipped = np.flip(obj, axis=1)
 
-a_flipped_fft = np.fft.fft2(a_flipped)
-
-
+# Perform the FFT of the flipped object
+obj_flipped_fft = np.fft.fft2(obj_flipped)
 
 # Plot the FFT of the flipped object
-a_flipped_fft_fig, a_flipped_fft_ax = plot_fft(a_flipped_fft, 'FFT of Flipped Object')
-a_flipped_fft_fig.show()
+obj_flipped_fft_fig, obj_flipped_fft_ax = plot_fft(obj_flipped_fft, 'FFT of Flipped Object Bitmap')
+obj_flipped_fft_fig.show()
 
+# Perform the FFT convolution in the frequency domain
+# This is done as the point-wise product of the FFT coefficients.
+convolution_freq = obj_flipped_fft * ws_fft
 
-pointwise_product =  a_flipped_fft * w_fft
+# Plot the convolution in the frequency domain
+convolution_freq_fig, convolution_freq_ax = plot_fft(convolution_freq, 'Convolution in Frequency Domain')
+convolution_freq_fig.show()
 
-# Plot the pointwise product
-pointwise_product_fig, pointwise_product_ax = plot_fft(pointwise_product, 'Pointwise Product')
-pointwise_product_fig.show()
+# Convert the convolution in the frequency domain back to the spatial domain
+# This is done through the inverse FFT
+convolution_spatial = np.fft.ifft2(convolution_freq)
 
-# %% Perform the inverse FFT
-
-convolution = np.fft.ifft2(pointwise_product)
-
-# Plot the convolution
-convolution_fig, convolution_ax = plot_fft(convolution, 'Convolution', display_boundary=True)
-
-# Color the cells that have a value of close to 0
-# convolution_ax.imshow(np.real(convolution) < 0.1, cmap='Blues', interpolation='none', alpha=0.5, extent=[0, convolution.shape[1], 0, convolution.shape[0]])
-
-
+# Plot the convolution in the spatial domain
+convolution_fig, convolution_ax = plot_fft(convolution_spatial, 'Convolution in the Spatial Domain')
 convolution_fig.show()
 
-merged_grids, merged_grids_rgb = merge_grids(w, w_rgb, a)
+# %% Now place the object in the workspace
 
-# merged_grids_fig, merged_grids_ax = plot_grid_rgb(merged_grids_rgb, 'Merged Grids RGB')
-# merged_grids_fig.show()
-#
-# merged_grids_fig, merged_grids_ax = plot_grid(merged_grids, 'Merged Grids')
-# merged_grids_fig.show()
+# Place the object in the workspace
+user_specified_offset = (0, 0)
+merged_grids = combine_bitmaps(obj, ws, user_specified_offset)
+
+# Plot the placed object in the workspace
+merged_grids_fig, merged_grids_ax = plot_bitmap(merged_grids, 'Merged Bitmaps')
+merged_grids_fig.show()
 
 
 
